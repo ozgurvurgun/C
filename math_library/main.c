4dddb5a9e0db379f5eb1cf2483a8d4eafbb431cb
Ozgur VURGUN <ozgurvurgun1@gmail.com>
2023-09-04 02:08:23 +0300

    math library added

---
+++ math_library/main.c
@@ -0,0 +1,25 @@
+#include <math.h>
+
+int main()
+{
+    //absolute
+    printf("%d\n", abs(-5));
+    int x = pow(2,5);
+
+    //pow
+    printf("%d\n", x);
+
+    //floor
+    printf("%f\n", floor(12.87)); //en yakin asagi tam sayi
+
+    //ceil
+    printf("%f\n", ceil(12.87));  //en yakin yukari yuvarlar
+
+    //square root
+    printf("%f\n", sqrt(81));
+
+    //E^2
+    printf("%f\n", exp(2));
+
+    return 0;
+}
