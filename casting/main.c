cb05357b6bb94b00a74a20556f96377f61ffb9b9
Ozgur VURGUN <ozgurvurgun1@gmail.com>
2023-09-04 02:18:49 +0300

    casting added

---
+++ casting/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x = 10;
+    int y = 4;
+
+    float z = (float) x / y;
+    printf("%f", z);
+
+    //diger veri tipleri icinde kullanim ayni
+    /*
+    isleme giren sayiladan en az biri ondalik sayi olursa
+    casting islemine gerek kalmaz C bunu ondalikli islem
+    olarak ele alir
+    */
+}
