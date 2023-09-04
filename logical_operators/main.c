2a58b61ae68901b0152f6b0a2e8a79716aaaced3
Ozgur VURGUN <ozgurvurgun1@gmail.com>
2023-09-04 04:04:38 +0300

    logical_operators added

---
+++ logical_operators/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   //if-else
+
+   int note = 50;
+
+   if(note >= 50){
+    printf("passed the exam");
+   }
+   else{
+    printf("failed the exam");
+   }
+
+    //operatorler bidigin diger dillerden farkli degil uzatmaya gerek yok
+
+
+}
